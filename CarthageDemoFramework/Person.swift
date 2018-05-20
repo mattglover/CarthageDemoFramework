5e59ce80fdff74044b823318e43fac7730788110
Matt Glover <matt@duchysoftware.com>
2018-05-20 16:20:13 +0100

    Updated Person.swift with dateOfBirth and daysAlive method

--- CarthageDemoFramework/Person.swift
+++ CarthageDemoFramework/Person.swift
@@ -1,12 +1,20 @@
 import Foundation
 
 public struct Person {
-	public let forename: String
-	public let surname: String
+	private(set) var forename: String
+	private(set) var surname: String
+	public var dateOfBirth: Date?
 
 	public init(forename: String, surname: String) {
 		self.forename = forename
 		self.surname = surname
+	}
+
+	public func daysAlive() -> Int? {
+		guard let dateOfBirth = self.dateOfBirth else { return nil }
+		let daysAliveSeconds = Int(Date().timeIntervalSince(dateOfBirth))
+		
+		return daysAliveSeconds / 86400
 	}
 }
 
--- CarthageDemoFrameworkTests/PersonTests.swift
+++ CarthageDemoFrameworkTests/PersonTests.swift
@@ -9,4 +9,18 @@ class PersonTests: XCTestCase {
 		sut = Person(forename: "Matt", surname: "Glover")
 		XCTAssertEqual("Matt Glover", sut.description)
 	}
+
+	func testAfterSettingDOB_getDaysAlive_returnsCorrectValue() {
+		sut = Person(forename: "Matt", surname: "Glover")
+
+		sut.dateOfBirth = Date.yesterday()
+		XCTAssertEqual(1, sut.daysAlive())
+	}
+}
+
+// MARK: - Test Helpers
+extension Date {
+	fileprivate static func yesterday() -> Date {
+		return Date().addingTimeInterval(-86400)
+	}
 }
