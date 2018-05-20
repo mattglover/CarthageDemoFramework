6b34b81cc77290335a099c35af7b2c436868c3f1
Matt Glover <matt@duchysoftware.com>
2018-05-20 15:50:36 +0100

    Added public initialiser for Person.swift

--- CarthageDemoFramework/Person.swift
+++ CarthageDemoFramework/Person.swift
@@ -3,6 +3,11 @@ import Foundation
 public struct Person {
 	public let forename: String
 	public let surname: String
+
+	public init(forename: String, surname: String) {
+		self.forename = forename
+		self.surname = surname
+	}
 }
 
 extension Person : CustomStringConvertible {
