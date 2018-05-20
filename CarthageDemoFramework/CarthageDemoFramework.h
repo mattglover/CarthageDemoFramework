556ba6255654e39077d1c863a7ef3a2282b4c51e
Matt Glover <matt@duchysoftware.com>
2018-05-20 14:59:14 +0100

    Initial Commit

---
+++ CarthageDemoFramework/CarthageDemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  CarthageDemoFramework.h
+//  CarthageDemoFramework
+//
+//  Created by Matt Glover on 20/05/2018.
+//  Copyright © 2018 Matt Glover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageDemoFramework.
+FOUNDATION_EXPORT double CarthageDemoFrameworkVersionNumber;
+
+//! Project version string for CarthageDemoFramework.
+FOUNDATION_EXPORT const unsigned char CarthageDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageDemoFramework/PublicHeader.h>
+
+
